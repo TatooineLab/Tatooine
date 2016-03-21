f1fc83ae9b5ca4f3a783b981606a94f038832e71
Ivan Spirdionov <spiridonov@tatooinelab.ru>
2016-03-21 19:54:58 +0300

    Image.pm: fix for saving undefined values

--- Image.pm
+++ Image.pm
@@ -123,7 +123,7 @@ sub registerImageActions {
 
 				# Присваиваем значения undef пустым строкам
 				foreach my $key (keys %fields){
-					$fields{$key} = undef unless defined $fields{$key};
+					$fields{$key} = undef if !$fields{$key} and $fields{$key} ne '0';
 				}
 
 				# Редактирование записи
